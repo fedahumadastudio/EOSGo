4a54f3a580bdb66659691d44930c043c8e33aa1b
Federico Ahumada <ahufede@gmail.com>
2024-07-27 20:33:32 -0300

    Menu create-find-join session functionality

---
+++ Source/EOSGo/Private/UI/GoMenu.cpp
@@ -0,0 +1,143 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/GoMenu.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+#include "Components/Button.h"
+#include "Subsystem/GoSubsystem.h"
+
+bool UGoMenu::Initialize()
+{
+	if (!Super::Initialize()) return false;
+
+	if (Host_Button)
+	{
+		Host_Button->OnClicked.AddDynamic(this, &UGoMenu::HostButtonClicked);
+	}
+	if (Join_Button)
+	{
+		Join_Button->OnClicked.AddDynamic(this, &UGoMenu::JoinButtonClicked);
+	}
+	return true;
+}
+
+void UGoMenu::NativeDestruct()
+{
+	MenuTearDown();
+	Super::NativeDestruct();
+}
+
+void UGoMenu::MenuSetup(int32 NumberOfPublicConnections, FString TypeOfMatch, FString LevelPath)
+{
+	MapToTravel = LevelPath.Append(FString(TEXT("?Listen")));
+	MatchType = TypeOfMatch;
+	NumberOfConnections = NumberOfPublicConnections;
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+	SetIsFocusable(true);
+
+	if (const UWorld* World = GetWorld())
+	{
+		if (APlayerController* PlayerController = World->GetFirstPlayerController())
+		{
+			FInputModeUIOnly InputModeData;
+			InputModeData.SetWidgetToFocus(TakeWidget());
+			InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+			PlayerController->SetInputMode(InputModeData);
+			PlayerController->SetShowMouseCursor(true);
+		}
+	}
+	
+	if (const UGameInstance* GameInstance = GetGameInstance())
+	{
+		GoSubsystem = GameInstance->GetSubsystem<UGoSubsystem>();
+	}
+
+	//~ Bind session callbacks.
+	if (IsValid(GoSubsystem))
+	{
+		GoSubsystem->GoOnCreateSessionComplete.AddDynamic(this, &UGoMenu::OnCreateSession);
+		GoSubsystem->GoOnFindSessionsComplete.AddUObject(this, &UGoMenu::OnFindSessions);
+		GoSubsystem->GoOnJoinSessionComplete.AddUObject(this, &UGoMenu::OnJoinSession);
+		GoSubsystem->GoOnDestroySessionComplete.AddDynamic(this, &UGoMenu::OnDestroySession);
+		GoSubsystem->GoOnStartSessionComplete.AddDynamic(this, &UGoMenu::OnStartSession);
+	}
+}
+
+void UGoMenu::OnCreateSession(bool bWasSuccessful)
+{
+	if (bWasSuccessful)
+	{
+		ScreenMessage(FColor::Green,FString(TEXT("Session created successfully")));
+		if (UWorld* World = GetWorld()) World->ServerTravel(MapToTravel);
+	}
+	else
+	{
+		ScreenMessage(FColor::Red,FString(TEXT("Failed creating session")));
+	}
+}
+
+void UGoMenu::OnFindSessions(const TArray<FOnlineSessionSearchResult>& SessionResults, bool bWasSuccessful)
+{
+	if (!IsValid(GoSubsystem)) return;
+	
+	for (auto Result : SessionResults)
+	{
+		FString SettingsValue;
+		Result.Session.SessionSettings.Get(FName("MatchType"), SettingsValue);
+		if (SettingsValue == MatchType)
+		{
+			GoSubsystem->GoJoinSession(Result);
+			return;
+		}
+	}
+}
+
+void UGoMenu::OnJoinSession(EOnJoinSessionCompleteResult::Type Result)
+{
+	if (IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
+	{
+		IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+		if (SessionInterface.IsValid())
+		{
+			FString Address;
+			SessionInterface->GetResolvedConnectString(NAME_GameSession, Address);
+			if (APlayerController* PlayerController = GetGameInstance()->GetFirstLocalPlayerController())
+			{
+				PlayerController->ClientTravel(Address, TRAVEL_Absolute);
+			}
+		}
+	}
+}
+
+void UGoMenu::OnDestroySession(bool bWasSuccessful)
+{
+}
+
+void UGoMenu::OnStartSession(bool bWasSuccessful)
+{
+}
+
+void UGoMenu::HostButtonClicked()
+{
+	if (GoSubsystem) GoSubsystem->GoCreateSession(NumberOfConnections, MatchType);
+}
+
+void UGoMenu::JoinButtonClicked()
+{
+	if (GoSubsystem) GoSubsystem->GoFindSessions(100);
+}
+
+void UGoMenu::MenuTearDown()
+{
+	RemoveFromParent();
+	if (const UWorld* World = GetWorld())
+	{
+		if (APlayerController* PlayerController = World->GetFirstPlayerController())
+		{
+			const FInputModeGameOnly InputData;
+			PlayerController->SetInputMode(InputData);
+		}
+	}
+}
---
+++ Source/EOSGo/Public/UI/GoMenu.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Blueprint/UserWidget.h"
+#include "GoMenu.generated.h"
+class UGoSubsystem;
+class UButton;
+
+/**
+ * 
+ */
+UCLASS()
+class EOSGO_API UGoMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected: //virtual
+	virtual bool Initialize() override;
+	virtual void NativeDestruct() override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category="EOS-Go|Misc")
+	void MenuSetup(int32 NumberOfPublicConnections = 3, FString TypeOfMatch = FString(TEXT("Lobby")), FString LevelPath = FString(TEXT("/Game/Maps/LobbyMap")));
+
+protected:
+	//~ Session callbacks for the custom delegates on the GoSubsystem.
+	UFUNCTION()
+	void OnCreateSession(bool bWasSuccessful);
+	void OnFindSessions(const TArray<FOnlineSessionSearchResult>& SessionResults, bool  bWasSuccessful);
+	void OnJoinSession(EOnJoinSessionCompleteResult::Type Result);
+	UFUNCTION()
+	void OnDestroySession(bool bWasSuccessful);
+	UFUNCTION()
+	void OnStartSession(bool bWasSuccessful);
+	
+private:
+	TObjectPtr<UGoSubsystem> GoSubsystem; //The subsystem designed to handle online sessions' functionality.
+	FString MapToTravel;
+	FString MatchType;
+	int32 NumberOfConnections;
+	void MenuTearDown();
+	
+	UPROPERTY(meta = (BindWidget))
+	UButton* Host_Button;
+	UPROPERTY(meta = (BindWidget))
+	UButton* Join_Button;
+
+	UFUNCTION()
+	void HostButtonClicked();
+	UFUNCTION()
+	void JoinButtonClicked();
+
+	
+
+	
+	
+};
